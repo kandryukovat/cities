90310fc43988a650146ff5da3648840f196ff02a
tatyana nosova <kandryukovat@gmail.com>
2023-09-20 20:50:14 +0700

    added processing of arguable cities

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
 from settings import Settings
 
 
-def get_new_word(letter, cities_list, already_used_words):#TODO: add checking on arguable cities
+def get_new_word(letter, cities_list, already_used_words, arguable):
     """gets a new word from the list that starts from the specified letter"""
     next_word = ''
     for word in cities_list:
-        if word[0].lower() == letter.lower():
+        if word[0].lower() == letter.lower() and word.lower() not in arguable:
             next_word = word
             break
     if next_word != '':
         update_word_lists(word, cities_list, already_used_words)
     return (next_word, cities_list, already_used_words)
 
-def check_input_word(last_letter, input_word, cities_list, already_used_words, stop_words):#TODO: check the win conditions
+def check_input_word(last_letter, input_word, cities_list, already_used_words, stop_words, arguable):#TODO: check the lose conditions
     """checks if the word is a valid city name and was not used yet"""
     answer = ''
-    if input_word.lower() in stop_words:
+    input_word = input_word.lower()
+    if input_word in stop_words:
         return 'stop'
-    if last_letter.lower() != input_word[0].lower():
+    if last_letter.lower() != input_word[0]:
         if last_letter != '':
             return 'letter'
     for word in already_used_words:
-        if word.lower() == input_word.lower():
+        if word.lower() == input_word:
             answer = 'already'
             break
     if answer == '':
         for word in cities_list:
-            if word.lower() == input_word.lower():
+            if word.lower() == input_word:
                 answer = 'valid'
                 update_word_lists(word, cities_list, already_used_words)
+                break
+        for a in arguable:
+            if a == input_word:
+                answer = 'arguable'
                 break
     return answer
 
@@ -44,19 +49,29 @@ def get_last_letter(word):
     return last_letter
 
 def check_arguable(word, arguable):
+    word_lower = word.lower()
     for a in arguable:
-        if a.lower() == word.lower():
+        if a.lower() == word_lower:
             return True
     return False
+
+
 if __name__ == "__main__":
     s = Settings()
 
     stop_words = s.get_stop_words()
     all_city_pairs = s.get_cities()
-    arguable = s.get_arguables()
     sayings = s.get_sayings()
     arguable_msg = s.get_arguable_saying()
     use_arguable = s.get_use_arguable()
+    if use_arguable == True:
+        arguable = []
+    else:
+        arguable_initial = s.get_arguables()
+        arguable = []
+        for word in arguable_initial:
+            arguable.append(word.lower())
+
 
     cities_list = []
     already_used_words = []
@@ -71,7 +86,7 @@ if __name__ == "__main__":
         answer = ''
         while not answer == 'valid':
             word = input()
-            answer = check_input_word(last_letter, word, cities_list, already_used_words, stop_words)
+            answer = check_input_word(last_letter, word, cities_list, already_used_words, stop_words, arguable)
             if answer == 'stop':
                 raise SystemExit
             elif answer == '':
@@ -80,14 +95,14 @@ if __name__ == "__main__":
                 print(sayings['already_was_message'])
             elif answer == 'letter':
                 print(sayings['wrong_letter_message'], last_letter)
+            elif answer == 'arguable':
+                print(arguable_msg)
             elif answer == 'valid':
-                if use_arguable == False:
-                    if check_arguable(word, arguable):
-                        answer = 'arguable'
-                        print(arguable_msg)
+                pass
 
         last_letter = get_last_letter(word)
-        answer = get_new_word(last_letter, cities_list, already_used_words)
+
+        answer = get_new_word(last_letter, cities_list, already_used_words, arguable)
         computers_word = answer[0]
         if computers_word == '':
             print(s.get_win_sayng(last_letter))
--- test.py
+++ test.py
@@ -1,37 +1,48 @@
 import unittest
 import main as game
 
+
 class GameTestCase(unittest.TestCase):
     def setUp(self):
-        self.cities_list = []
-
+        #self.cities_list = []
+        self.arguable = []
+        self.stops = ['0', 'стоп', 'хватит']
 
     def test_get_new_word_ru_1(self):
         """if an appropriate city name exists"""
         cities_list = ['Анапа', 'Агрыз', 'Брянск', 'Новосибирск', 'Москва']
         already_used_words = []
         letter = 'а'
-        res = game.get_new_word(letter, cities_list, already_used_words)
+        res = game.get_new_word(letter, cities_list, already_used_words, self.arguable)
         self.assertEqual(res, ('Анапа', ['Агрыз', 'Брянск', 'Новосибирск', 'Москва'], ['Анапа']))
-
 
     def test_get_new_word_ru_2(self):
         """if an appropriate city name doesn't exist"""
         cities_list = ['Анапа', 'Брянск', 'Новосибирск', 'Москва']
         already_used_words = ['Омск']
         letter = 'в'
-        res = game.get_new_word(letter, cities_list, already_used_words)
+        res = game.get_new_word(letter, cities_list, already_used_words, self.arguable)
         self.assertEqual(res, ('', ['Анапа', 'Брянск', 'Новосибирск', 'Москва'], ['Омск']))
 
-    def test_check_input_word_ru_1(self):#check_input_word(last_letter, input_word, cities_list, already_used_words, stop_words)
+
+    def test_get_new_word_ru_3(self):
+        """if the next word in the list is arguable"""
+        cities_list = ['Алушта', 'Анапа', 'Брянск', 'Новосибирск', 'Москва']
+        arguable = ['алушта', 'бахчисарай']#с маленькой буквы надо
+        already_used_words = ['Омск']
+        letter = 'а'
+        res = game.get_new_word(letter, cities_list, already_used_words, arguable)
+        self.assertEqual(res, ('Анапа', ['Алушта', 'Брянск', 'Новосибирск', 'Москва'], ['Омск', 'Анапа']))
+
+    def test_check_input_word_ru_1(self):
         """if the first letter of an input word is wrong"""
         last_letter = 'б'
         input_word = 'анапа'
         cities_list = ['Брянск', 'Москва']
         already_used_words = []
-        stop_words = []
-        answer = game.check_input_word(last_letter, input_word, cities_list, already_used_words, stop_words)
-        self.assertEqual(answer,'letter')
+        answer = game.check_input_word(last_letter, input_word, cities_list, already_used_words, self.stops,
+                                       self.arguable)
+        self.assertEqual(answer, 'letter')
 
     def test_check_input_word_ru_2(self):
         """if the input word was already used"""
@@ -39,10 +50,9 @@ class GameTestCase(unittest.TestCase):
         input_word = 'бийск'
         cities_list = ['Брянск', 'Москва']
         already_used_words = ['Бийск']
-        stop_words = []
-        answer = game.check_input_word(last_letter, input_word, cities_list, already_used_words, stop_words)
-        self.assertEqual(answer,'already')
-
+        answer = game.check_input_word(last_letter, input_word, cities_list, already_used_words, self.stops,
+                                       self.arguable)
+        self.assertEqual(answer, 'already')
 
     def test_check_input_word_ru_3(self):
         """if the input word is not in the list"""
@@ -50,10 +60,9 @@ class GameTestCase(unittest.TestCase):
         input_word = 'бийск'
         cities_list = ['Брянск', 'Москва']
         already_used_words = []
-        stop_words = []
-        answer = game.check_input_word(last_letter, input_word, cities_list, already_used_words, stop_words)
-        self.assertEqual(answer,'')
-
+        answer = game.check_input_word(last_letter, input_word, cities_list, already_used_words, self.stops,
+                                       self.arguable)
+        self.assertEqual(answer, '')
 
     def test_check_input_word_ru_4(self):
         """if the input word is in the list"""
@@ -61,9 +70,9 @@ class GameTestCase(unittest.TestCase):
         input_word = 'брянск'
         cities_list = ['Брянск', 'Москва']
         already_used_words = []
-        stop_words = []
-        answer = game.check_input_word(last_letter, input_word, cities_list, already_used_words, stop_words)
-        self.assertEqual(answer,'valid')
+        answer = game.check_input_word(last_letter, input_word, cities_list, already_used_words, self.stops,
+                                       self.arguable)
+        self.assertEqual(answer, 'valid')
 
 
     def test_check_input_word_ru_5(self):
@@ -72,10 +81,9 @@ class GameTestCase(unittest.TestCase):
         input_word = 'БрЯнСК'
         cities_list = ['Брянск', 'Москва']
         already_used_words = []
-        stop_words = []
-        answer = game.check_input_word(last_letter, input_word, cities_list, already_used_words, stop_words)
-        self.assertEqual(answer,'valid')
-
+        answer = game.check_input_word(last_letter, input_word, cities_list, already_used_words, self.stops,
+                                       self.arguable)
+        self.assertEqual(answer, 'valid')
 
     def test_check_input_word_ru_6(self):
         """if the input word is a stop word """
@@ -84,9 +92,21 @@ class GameTestCase(unittest.TestCase):
         cities_list = ['Брянск', 'Москва']
         already_used_words = []
         stop_words = ['0', 'стоп', 'хватит']
-        answer = game.check_input_word(last_letter, input_word, cities_list, already_used_words, stop_words)
-        self.assertEqual(answer,'stop')
+        answer = game.check_input_word(last_letter, input_word, cities_list, already_used_words, stop_words,
+                                       self.arguable)
+        self.assertEqual(answer, 'stop')
 
+
+    def test_check_input_word_ru_7(self):
+        """if the input word is an arguable city name """
+        last_letter = 'б'
+        input_word = 'бахчисарай'
+        cities_list = ['Брянск', 'Бахчисарай', 'Москва']
+        already_used_words = []
+        arguable = ['алушта', 'бахчисарай']
+        answer = game.check_input_word(last_letter, input_word, cities_list, already_used_words, self.stops,
+                                       arguable)
+        self.assertEqual(answer, 'arguable')
 
     def test_get_last_letter_ru_1(self):
         """if the last letter is big"""
@@ -94,13 +114,11 @@ class GameTestCase(unittest.TestCase):
         letter = game.get_last_letter(word)
         self.assertEqual(letter, 'к')
 
-
     def test_get_last_letter_ru_2(self):
         """if the last letter is weird"""
         word = 'грязь'
         letter = game.get_last_letter(word)
         self.assertEqual(letter, 'з')
-
 
     def test_get_last_letter_ru_3(self):
         """if the last letter is usual"""
@@ -108,14 +126,12 @@ class GameTestCase(unittest.TestCase):
         letter = game.get_last_letter(word)
         self.assertEqual(letter, 'а')
 
-
     def test_check_arguable_ru_1(self):
         """if the word is not in arguable list"""
         word = 'Омск'
         arguable = ['Анапа', 'Москва']
         res = game.check_arguable(word, arguable)
         self.assertEqual(res, False)
-
 
     def test_check_arguable_ru_2(self):
         """if the word is in arguable list, small case"""
@@ -130,3 +146,10 @@ class GameTestCase(unittest.TestCase):
         arguable = ['Анапа', 'Москва']
         res = game.check_arguable(word, arguable)
         self.assertEqual(res, True)
+
+    def test_check_arguable_ru_4(self):
+        """if the arguable list is empty"""
+        word = 'Анапа'
+        arguable = []
+        res = game.check_arguable(word, arguable)
+        self.assertEqual(res, False)
